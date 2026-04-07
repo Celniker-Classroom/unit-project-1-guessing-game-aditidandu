a28ac091f14e39ead5a8226f21a5d8d5d699657a
aditidandu <70790422+aditidandu@users.noreply.github.com>
2026-04-07 20:53:27 +0000

    disabled play button and tried adding msg

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let scores = 0;
 
 //Player Name
 let playerName = prompt("Enter your name:");
-playerName = playerName.toUpperCase();
+// playerName = playerName.toUpperCase();
 
 
 //Play
@@ -20,9 +20,18 @@ document.getElementById("playBtn").addEventListener("click", function(){
             range = parseint(radios[i].value);
         }
     }
+
+    //pick answer
+    answer = Math.floor(Math.random() * range) + 1;
+    //Disable and Enable buttons and radio choices
+    document.getElementById("msg").textContent = playerName + ", guess a number between 1 and " + range;
+    document.getElementById("guess").value="";
+    document.getElementById("guessBtn").disabled = false;
+    document.getElementById("giveUpBtn").disabled = false;
+    document.getElementById("playBtn").disabled = true;
+
+    let levelRadios = document.getElementsByName("level");
+    for (let i = 0; i <levelRadios.length; i++){
+        levelRadios[i].disabled = true;
+    }
 })
-
-//round setup
-answer = Math.floor(Math.random() * range) + 1;
-
-document.getElementById("msg").textContent = playerName + ", guess a number between 1 and " + range;
